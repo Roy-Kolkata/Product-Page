e8f0d3d93f1608dc1d6840aaad026b14155bff5e
Roy-Kolkata <roy2004akash@gmail.com>
2025-07-07 12:21:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.jsx"
+import Navigation from "./components/Navigation.jsx";
+import Hero from "./components/Hero.jsx";
+const App =()=>{
+  return (
+    <div>
+        <Navigation/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default App;
